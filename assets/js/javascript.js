9f84faabf3faca494da583d017dd91bb046da8ed
MJ-Web3 <103333893+MJ-Web3@users.noreply.github.com>
2022-04-10 02:12:38 +0200

    Add files via upload

---
+++ assets/js/javascript.js
@@ -0,0 +1,96 @@
+let dark = "rgb(51 51 51)";
+let yellow = "rgb(240 133 51)";
+
+function linkedin_effect(){
+    let linkedin = document.getElementById("linkedin")
+    let linkedin_i = document.getElementById("linkedin_i")
+    
+    linkedin.style.backgroundColor=yellow;
+    linkedin_i.style.backgroundColor=yellow;
+
+}
+
+function linkedin_normal(){
+    let linkedin = document.getElementById("linkedin")
+    let linkedin_i = document.getElementById("linkedin_i")
+    
+    linkedin.style.backgroundColor=dark;
+    linkedin_i.style.backgroundColor=dark;
+
+}
+
+
+function twitter_effect(){
+    let twitter = document.getElementById("twitter")
+    let twitter_i = document.getElementById("twitter_i")
+    
+    twitter.style.backgroundColor=yellow;
+    twitter_i.style.backgroundColor=yellow;
+
+}
+
+function twitter_normal(){
+    let twitter = document.getElementById("twitter")
+    let twitter_i = document.getElementById("twitter_i")
+    
+    twitter.style.backgroundColor=dark;
+    twitter_i.style.backgroundColor=dark;
+
+}
+
+
+function pinterest_effect(){
+    let pinterest = document.getElementById("pinterest")
+    let pinterest_i = document.getElementById("pinterest_i")
+    
+    pinterest.style.backgroundColor=yellow;
+    pinterest_i.style.backgroundColor=yellow;
+
+}
+
+function pinterest_normal(){
+    let pinterest = document.getElementById("pinterest")
+    let pinterest_i = document.getElementById("pinterest_i")
+    
+    pinterest.style.backgroundColor=dark;
+    pinterest_i.style.backgroundColor=dark;
+
+}
+
+
+function googlePlus_effect(){
+    let googlePlus = document.getElementById("google-plus")
+    let googlePlus_i = document.getElementById("google-plus_i")
+    
+    googlePlus.style.backgroundColor=yellow;
+    googlePlus_i.style.backgroundColor=yellow;
+
+}
+
+function googlePlus_normal(){
+    let googlePlus = document.getElementById("google-plus")
+    let googlePlus_i = document.getElementById("google-plus_i")
+    
+    googlePlus.style.backgroundColor=dark;
+    googlePlus_i.style.backgroundColor=dark;
+
+}
+
+
+function rss_effect(){
+    let rss = document.getElementById("rss")
+    let rss_i = document.getElementById("rss_i")
+    
+    rss.style.backgroundColor=yellow;
+    rss_i.style.backgroundColor=yellow;
+
+}
+
+function rss_normal(){
+    let rss = document.getElementById("rss")
+    let rss_i = document.getElementById("rss_i")
+    
+    rss.style.backgroundColor=dark;
+    rss_i.style.backgroundColor=dark;
+
+}
